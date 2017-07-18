00e894dff698cc9ff29ed2a8cda02afbba151351
Nguyen Tuan Anh <anhnt796@gmail.com>
2017-07-18 11:36:24 +0700

    add some comments

--- app/src/main/java/dev/anhnt/kimdung/activities/ContentActivity.java
+++ app/src/main/java/dev/anhnt/kimdung/activities/ContentActivity.java
@@ -114,6 +114,11 @@ public class ContentActivity extends AppCompatActivity implements NavigationView
         }
     }
 
+    /**
+     * Phương thức này xóa html tags trong một đoạn string
+     * @param html Dữ liệu vào
+     * @return Đoạn string không chứa html tags
+     */
     private String stripHtml(String html) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY).toString();
@@ -122,6 +127,11 @@ public class ContentActivity extends AppCompatActivity implements NavigationView
         }
     }
 
+    /**
+     * Phương thức này tách các từ trong một đoạn văn thành một tập hợp (các từ không trùng lặp).
+     * @param text Đoạn văn bản đầu vào
+     * @return Tập hợp các từ trong đoạn văn
+     */
     private static Set<String> getWords(String text) {
         Set<String> words = new HashSet<>();
         BreakIterator breakIterator = BreakIterator.getWordInstance();
--- app/src/main/java/dev/anhnt/kimdung/activities/IntroduceActivity.java
+++ app/src/main/java/dev/anhnt/kimdung/activities/IntroduceActivity.java
@@ -175,6 +175,11 @@ public class IntroduceActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    /**
+     * Phương thức này xóa html tags trong một đoạn string
+     * @param html Dữ liệu vào
+     * @return Đoạn string không chứa html tags
+     */
     private String stripHtml(String html) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             return Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY).toString();
@@ -183,6 +188,11 @@ public class IntroduceActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    /**
+     * Phương thức này tách các từ trong một đoạn văn thành một tập hợp (các từ không trùng lặp).
+     * @param text Đoạn văn bản đầu vào
+     * @return Tập hợp các từ trong đoạn văn
+     */
     private static Set<String> getWords(String text) {
         Set<String> words = new HashSet<>();
         BreakIterator breakIterator = BreakIterator.getWordInstance();
@@ -202,8 +212,8 @@ public class IntroduceActivity extends AppCompatActivity implements View.OnClick
         int numErr = 0;
         List<String> listResult = new ArrayList<>();
         removeCheck();
-        String str_without_html = stripHtml(text);
-        Set<String> words = getWords(str_without_html);
+        String stringWithoutHtml = stripHtml(text);
+        Set<String> words = getWords(stringWithoutHtml);
         for (String word: words) {
             if (!dict.hasWord(word) && !StringUtils.isNumeric(word)) {
                 numErr++;
